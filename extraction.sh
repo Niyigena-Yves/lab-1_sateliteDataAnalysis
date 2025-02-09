7e24cd9e6e4ac1d8e90a12c401f00d6c469c183a
mutaganzwa-desire <d.mutaganzw@alustudent.com>
2025-02-09 00:10:03 -0800

    Updated extraction script

--- extraction.sh
+++ extraction.sh
@@ -2,4 +2,4 @@
 #Extract the top 10 highest temperatures from satelite_temperature_data.csv
 sort -t, -k3 -nr raw_data/satelite_temperature_data.csv | head -n 10 > analyzed_data/highest_temp.csv
 #Extract the entire data from a country of your choice from satelite_temperature_data.csv and save the result in descending order of humidity (highest to lowest)
-grep Angola satellite_temperature_data.csv | sort -t, -k4,nr > analyzed_data/humidity_data_Angola.csv
+grep Angola raw_data/satelite_temperature_data.csv | sort -t, -k4 -nr > analyzed_data/humidity_data_Angola.csv
